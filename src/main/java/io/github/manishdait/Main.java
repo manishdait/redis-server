b75e997a5955b09ff4186aeb4878b4ec735bf051
Manish Dait <daitmanish88@gmail.com>
2025-09-22 20:43:44 +0530

    implemented simple commands

--- src/main/java/io/github/manishdait/Main.java
+++ src/main/java/io/github/manishdait/Main.java
@@ -3,31 +3,18 @@ package io.github.manishdait;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+  static final Map<String, Object> MAP = new HashMap<>();
+
   public static void main(String[] args) throws IOException {
     ServerSocket server = new ServerSocket(6379);
 
     while (true) {
       Socket socket = server.accept();
-
-      Object object = new RespDeserializer(socket.getInputStream()).decode();
-  
-
-      if (object instanceof Object[]) {
-        Object[] cmd =  (Object[]) object;
-
-        for (Object obj : cmd) { 
-          System.out.println(obj);
-        }
-
-        if (cmd[0].equals("PING")) {
-          socket.getOutputStream().write(RespSerializer.toSimpleString("PONG").getBytes(StandardCharsets.UTF_8));
-        }
-      }
-
-      socket.close();
+      new Thread(new SocketHandler(socket)).start();
     }
   }
 }
